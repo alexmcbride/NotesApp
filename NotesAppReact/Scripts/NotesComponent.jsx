45c53cac53112718214f8051b3ee7513e7761b81
Alex McBride <xandermcbride@gmail.com>
2018-02-04 14:54:11 +0000

    Added router

--- NotesAppReact/Scripts/NotesComponent.jsx
+++ NotesAppReact/Scripts/NotesComponent.jsx
@@ -1,8 +1,18 @@
 ﻿
+// Routing stuff
+var content = document.getElementById("content");
+var router = new Navigo(null, true, '#!');
+
 class Note extends React.Component {
+    onSelectNote(evt) {
+        router.navigate('/notes/' + this.props.noteid);
+
+        evt.preventDefault();
+    }
+
     render() {
         return <div className="note">
-            <h2>{this.props.title}</h2>
+            <h2><a href="#" onClick={this.onSelectNote.bind(this)}>{this.props.title}</a></h2>
             <p>{this.props.content}</p>
         </div>;
     }
@@ -13,7 +23,7 @@ class NotesList extends React.Component {
         const data = this.props.data;
 
         const noteList = data.map((note) =>
-            <Note key={note.Id} title={note.Title} content={note.Content} />
+            <Note key={note.Id} noteid={note.Id} title={note.Title} content={note.Content} />
         );
 
         return <div className="notes-list">{noteList}</div>;
@@ -52,4 +62,38 @@ class NotesComponent extends React.Component {
     }
 }
 
-ReactDOM.render(<NotesComponent />, document.getElementById("content"));
+class NoteComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { note: {} };
+    }
+
+    componentDidMount() {
+        // Get JSON from API and set as state.
+        const url = "http://localhost:51781/api/notes/" + this.props.id;
+        $.getJSON(url, function (data) {
+            console.log(data);
+            this.setState({ note: data });
+        }.bind(this));
+    }
+
+    onGoBack(evt) {
+        router.navigate();
+    }
+
+    render() {
+        return <div className="note-component">
+            <Note noteid={this.state.note.Id} title={this.state.note.Title} content={this.state.note.Content} />
+
+            <form>
+                <input type="button" value="Go back..." onClick={this.onGoBack} />
+            </form>
+        </div>
+    }
+}
+
+router.on('/notes/:id', function (params, query) {
+    ReactDOM.render(<NoteComponent id={params.id} />, content);
+}).on('*', function () {
+    ReactDOM.render(<NotesComponent />, content);
+}).resolve();
